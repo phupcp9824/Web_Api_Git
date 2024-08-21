31ba5b4a4f1791d4db98363d273f36ed85e77675
phupcp9824 <144216475+phupcp9824@users.noreply.github.com>
2024-08-21 22:50:42 +0700

    Add files via upload

---
+++ Web_Api_Git/Web_Api_Git/WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace Web_Api_Git
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32.0 + (float this.TemperatureC / 0.5556)
